6f77ac33c8bad9014818fd77aa0568351115e6b1
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-02-22 00:06:47 +0600

    applied button is added

---
+++ sctipts.js
@@ -0,0 +1,43 @@
+const job1 = document.getElementById('notapplied1');
+const job2 = document.getElementById('notapplied2');
+const job3 = document.getElementById('notapplied3');
+const job4 = document.getElementById('notapplied4');
+const job5 = document.getElementById('notapplied5');
+const job6 = document.getElementById('notapplied6');
+const job7 = document.getElementById('notapplied7');
+const job8 = document.getElementById('notapplied8');
+
+job1.addEventListener('click' , function(){
+    job1.innerText = 'Applied'
+    
+})
+job2.addEventListener('click' , function(){
+    job2.innerText = 'Applied'
+
+    
+})
+job3.addEventListener('click' , function(){
+    job3.innerText = 'Applied'
+    
+})
+job4.addEventListener('click' , function(){
+    job4.innerText = 'Applied'
+    
+})
+job5.addEventListener('click' , function(){
+    job5.innerText = 'Applied'
+    
+})
+job6.addEventListener('click' , function(){
+    job6.innerText = 'Applied'
+    
+})
+job7.addEventListener('click' , function(){
+    job7.innerText = 'Applied'
+    
+})
+
+job8.addEventListener('click' , function(){
+   job8.innerText = 'Applied'
+    
+})
